51123c1e0f3acc6fffe2c3e65ac1e949a8e4d896
fredy palma <fpalma@siccob.com.mx>
2021-06-24 12:52:50 -0500

    Init project

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <>
+        <h1>Nuevo Adist4</h1>
+    </>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname,'src/index.js'),
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    resolve : {
+        extensions: ['.js','.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/i,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template/index.html',
+        }),
+    ],
+};
